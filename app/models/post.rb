db59a1fdfbcbb21535df28cba676763ad2438dc8
kopo4eweb <kopo4e.web@yandex.ru>
2020-04-22 20:29:12 +0300

    add Tags to Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,18 @@
 class Post < ApplicationRecord
   validates :title, :summary, :body, presence: true
 
+  has_many :taggings
+  has_many :tags, through: :taggings
+
   mount_uploader :image, ImageUploader
+
+  def all_tags
+    self.tags.map(&:name).join(', ')
+  end
+
+  def all_tags=(names)
+    self.tags = names.split(',').map do |name|
+      Tag.where(name: name.strip).first_or_create
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'tags/show'
   root 'posts#index'
   resources :posts
   resources :pictures, only: [:create, :destroy]
+  resources :tags, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
