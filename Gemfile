d531a41440f34d5969ca975a237d3cac98a65c30
kopo4eweb <kopo4e.web@yandex.ru>
2020-04-25 14:56:50 +0300

    add gem Ancestry to Category for create tree structure categories

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'carrierwave', '~> 2.0'
 gem "mini_magick"
 gem 'trix-rails', require: 'trix'
 gem 'will_paginate', '~> 3.1.0'
+gem 'ancestry'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_25_104621) do
+ActiveRecord::Schema.define(version: 2020_04_25_113336) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "pictures", force: :cascade do |t|
