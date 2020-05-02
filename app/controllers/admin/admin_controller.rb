03a52f9492b3b30ebb90c35ae7fae1105cdda8eb
kopo4eweb <kopo4e.web@yandex.ru>
2020-05-02 21:28:29 +0300

    check authenticate user and role Admin for access Admin panel

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,3 +1,10 @@
 class Admin::AdminController < ApplicationController
   before_action :authenticate_user!
+  before_action :check_admin
+
+  protected
+
+  def check_admin
+    redirect_to root_path, alert: 'У вас не прав доступа к данной странице!' unless current_user.admin?
+  end
 end
