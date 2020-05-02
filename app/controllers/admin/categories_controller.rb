6f945a857f8ff06995a6f03e56150d340cfc3d8a
kopo4eweb <kopo4e.web@yandex.ru>
2020-05-02 20:10:01 +0300

    upd before action for authenticate user

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CategoriesController < ApplicationController
 
-  before_action :authenticate_user!, except: :show
+  before_action :authenticate_user!
   before_action :set_category, only: [:edit, :update, :destroy]
 
   def index
